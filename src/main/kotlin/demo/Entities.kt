6c4b6717eb98825c6b4502b514c68e18e6dac34f
mjedli <jedli.mejdi.recup@gmail.com>
2021-11-28 15:05:44 +0100

    my first kotlin project

---
+++ src/main/kotlin/demo/Entities.kt
@@ -0,0 +1,14 @@
+package demo
+
+import org.springframework.data.annotation.Id
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+
+@Entity
+class User (
+    var login: String,
+    var firstname: String,
+    var lastname: String,
+    var description: String? = null,
+    @Id @GeneratedValue var id: Long? = null
+)
---
+++ src/main/kotlin/demo/MainController.kt
@@ -0,0 +1,23 @@
+package demo
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class `MainController` (private val repository: UserRepository) {
+
+    @GetMapping("/")
+    fun blog(model: Model):String {
+        model["title"] = "My Application";
+        if(repository.findByLogin("myname")?.firstname != null) {
+            model["name"] = repository.findByLogin("myname")?.firstname!!;
+        } else {
+            model["name"] = "Default";
+        }
+
+        return "blog";
+    }
+
+}
