a5bead67e8b9e3d032860137cb595f2918116847
mjedli <jedli.mejdi.recup@gmail.com>
2021-11-28 16:37:17 +0100

    my first kotlin project

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -4,8 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
 
-fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+	fun main(args: Array<String>) {
+		runApplication<DemoApplication>(*args)
+	}
+
 }
--- src/main/kotlin/demo/MainController.kt
+++ src/main/kotlin/demo/MainController.kt
@@ -10,8 +10,10 @@ class `MainController` (private val repository: UserRepository) {
 
     @GetMapping("/")
     fun blog(model: Model):String {
+
         model["title"] = "My Application";
-        if(repository.findByLogin("myname")?.firstname != null) {
+
+        if (repository.findByLogin("myname")?.firstname != null) {
             model["name"] = repository.findByLogin("myname")?.firstname!!;
         } else {
             model["name"] = "Default";
